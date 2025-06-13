a83129c3ba9431dcf4bd22c5bfdf7be72fdd441c
Raul Otaño <rulyotano@gmail.com>
2025-06-13 21:25:53 +0200

    created new deck and print

--- projects/cards/main.go
+++ projects/cards/main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
-
 func main()  {
-	// var card string = "Ace of Spades"
-	card := newCard()
-	fmt.Println(card)
-}
-
-func newCard() string{
-	return "Five of Diamonds"
+	cards := newDeck()
+	
+	cards.print()
 }
