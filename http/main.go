824875f60e0c3fcac8cd41e9dd69eacb5ea533a0
Raul Otaño <rulyotano@gmail.com>
2025-07-25 20:50:02 +0200

    implementing custom writter

--- http/main.go
+++ http/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct {}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
+}
 
 func main () {
 	response, err := http.Get("https://google.com")
@@ -13,6 +21,23 @@ func main () {
 		fmt.Println("There was an error making the request:", err)
 		os.Exit(1)
 	}
+
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("The request was not successful. Status code:", response.StatusCode)
+		os.Exit(1)
+	}
+
+	lw := logWriter{}
+
+	io.Copy(lw, response.Body)
+	// buffer := make([]byte, 256)
+	// content := []byte{}
+	// nreaded, _ := response.Body.Read(buffer)
+
+	// for ; nreaded > 0; {
+	// 	content = append(content, buffer[:nreaded]...)
+	//   nreaded, _ = response.Body.Read(buffer)
+	// }
 	
-	fmt.Println(response)
+	// fmt.Println(string(content))
 }
